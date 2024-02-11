c631184f89e558b15fa5d3d554cbfd1ea7f2dc06
aderemilekun <adelefatai@yahoo.com>
2024-02-11 15:47:22 +0100

    Initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/rhemi7/bookings/pkg/config"
+	"github.com/rhemi7/bookings/pkg/config/models"
+	"github.com/rhemi7/bookings/pkg/render"
+)
+
+// The repository used by the handlers
+var Repo *Repository
+
+// This is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// Creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// newHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	stringMap := make(map[string]string)
+
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
